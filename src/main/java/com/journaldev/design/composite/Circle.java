6a9dd508e86f76a0251f2c15093f0e5ce8a23c2a
AD-ONE\pawan <pawan.mandhan@soprasteria.com>
2018-10-26 16:59:16 +0530

    composite pattern

---
+++ src/main/java/com/journaldev/design/composite/Circle.java
@@ -0,0 +1,9 @@
+package com.journaldev.design.composite;
+
+public class Circle implements Shape {
+
+	public void draw(String fillColor) {
+		System.out.println("Drawing Circle with color " + fillColor);
+	}
+
+}
---
+++ src/main/java/com/journaldev/design/composite/Triangle.java
@@ -0,0 +1,9 @@
+package com.journaldev.design.composite;
+
+public class Triangle implements Shape {
+
+	public void draw(String fillColor) {
+		System.out.println("Drawing Triangle with color " + fillColor);
+	}
+
+}
