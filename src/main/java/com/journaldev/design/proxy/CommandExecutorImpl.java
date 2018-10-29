e24aff1f6bf9a9a0817ac6ab98142f1904483cef
AD-ONE\pawan <pawan.mandhan@soprasteria.com>
2018-10-29 15:49:13 +0530

    proxy pattern

---
+++ src/main/java/com/journaldev/design/proxy/CommandExecutorImpl.java
@@ -0,0 +1,13 @@
+package com.journaldev.design.proxy;
+
+import java.io.IOException;
+
+public class CommandExecutorImpl implements CommandExecutor {
+
+	public void runCommand(String cmd) throws IOException {
+                //some heavy implementation
+		Runtime.getRuntime().exec(cmd);
+		System.out.println("'" + cmd + "' command executed.");
+	}
+
+}
